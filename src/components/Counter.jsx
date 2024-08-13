b510e15a28a18cbe14ce43ce5ce8fe5cc24ed14d
abrahamvsdev <abrahamvsdev@gmail.com>
2024-08-13 12:59:29 +0200

    Fin de projecto

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -4,9 +4,12 @@ export function Counter() {
     const [counter, setCounter] = useState(0)
     return(
         <>
-        <span text-yellow-300 text-xl mr-4 >{counter}</span>,
-        <button class="border px-4 py-2 text-xl"  onClick={()=>setCounter(counter=>counter + 1)}>+</button>
-        <button class="border px-4 py-2 text-xl"  onClick={()=>setCounter(counter=>counter - 1)}>-</button>
+        <div class="space-x-4 text-center text-xl mb-10">
+            <p class="text-center mb-4">Contadores de cohetes exitosos</p>
+            <button class="border px-4 py-2 text-xl text-green-400 text-center"  onClick={()=>setCounter(counter=>counter + 1)}>+</button>
+            <span class="text-yellow-300 text-xl mr-4" >{counter}</span>
+            <button class="border px-4 py-2 text-xl text-red-400"  onClick={()=>setCounter(counter=>counter - 1)}>-</button>
+        </div>
         </>
     )
 }
